216b0239e20f6fd2af47a1350dbec7f9c003228a
Marcy Knook <emknook@gmail.com>
2025-05-08 22:08:02 +0200

    Collision update

--- js/snake.js
+++ js/snake.js
@@ -1,7 +1,10 @@
-var tileSize = 20; //px
-var fieldSize = 40; //x & y (0 - 39)
-var fieldWidth = tileSize * fieldSize;
-var fieldHeight = tileSize * fieldSize;
+//px => make into variable defined by size of window
+var tileHeight = 20;
+var tileWidth = 20;
+var fieldSize = 30; //x & y (0 - 19)
+
+var fieldWidth = tileWidth * fieldSize;
+var fieldHeight = tileHeight * fieldSize;
 var snakeContainer = document.getElementById("snake");
 var field = document.createElement("canvas");
 
@@ -16,17 +19,17 @@ snakeContainer.querySelector('[id="content"]').append(field);
 var snake = [{ x: Math.floor(fieldSize / 2), y: Math.floor(fieldSize / 2) },
 { x: Math.floor(fieldSize / 2), y: Math.floor(fieldSize / 2) + 1 },
 { x: Math.floor(fieldSize / 2), y: Math.floor(fieldSize / 2) + 2 }];
-let dirX = 0; // -1 = left 1 = right
-let dirY = 1; // -1 = up 1 = down
+var dirX = 0; // -1 = left 1 = right
+var dirY = 1; // -1 = up 1 = down
 
-let score = 0;
+var score = 0;
 
 //setup first level contents
 var berries = [];
 addBerries(3, 4);//add at least 3 for first level
 function addBerries(min, max) {
-    //add at least min amount of berries, plus max - 1
-    for (var i = 0; i < Math.floor(Math.random() * max) + min; i++) {
+    //add at least min amount of berries, plus max
+    for (var i = 0; (i < Math.floor(Math.random() * max) + min) && i < max; i++) {
         var posX = Math.floor(Math.random() * fieldSize);
         var posY = Math.floor(Math.random() * fieldSize);
         if (!snake.find(e => e.x == posX && e.y == posY)) {
@@ -39,18 +42,50 @@ function addBerries(min, max) {
 function gameLoop() {
     //function
     moveSnake();
-
+    //determine size of field
+    determineSize();
     //draw
     cnvsCtx.clearRect(0, 0, fieldWidth, fieldHeight);
     drawBerries();
     drawSnake();
 }
 
+function determineSize() {
+    fieldHeight = snakeContainer.style.height;
+    fieldWidth = snakeContainer.style.width;
+    var width = fieldWidth.split("px")[0];
+    var height = fieldHeight.split("px")[0];
+    tileWidth = width / fieldSize;
+    tileHeight = height / fieldSize;
+    field.style.width = width + 'px';
+    field.style.height = height - 24 + 'px';
+    field.width = width;
+    field.height = height;
+
+}
+
 function handleCollision() {
+    //move Out of Screen
+    var head = snake[snake.length - 1];
+    if (head.x > fieldSize - 1 || head.y > fieldSize - 1 || head.x < 0 || head.y < 0) {
+        gameOver();
+        return;
+    }
+
+    //snake collision
+    for (var i = snake.length - 2; i >= 0; i--) {
+        let body = snake[i];
+        if (head.x == body.x && head.y == body.y) {
+            gameOver();
+            return;
+        }
+    }
+
+    //berry collision
     var berryToRemove = -1;
     for (var i = 0; i < berries.length; i++) {
         var berry = berries[i];
-        if (snake[snake.length - 1].x == berry.x && snake[snake.length - 1].y == berry.y) {
+        if (head.x == berry.x && head.y == berry.y) {
             score++;
             // scoreDiv.innerText = "Score: " + score;
             berryToRemove = i;
@@ -71,14 +106,18 @@ function handleCollision() {
 
 function drawSnake() {
     cnvsCtx.fillStyle = "white";
-    snake.forEach(e => cnvsCtx.fillRect(e.x * tileSize, e.y * tileSize, tileSize, tileSize));
+    snake.forEach(e => cnvsCtx.fillRect(e.x * tileWidth, e.y * tileHeight, tileWidth, tileHeight));
     cnvsCtx.strokeStyle = "black";
-    snake.forEach(e => cnvsCtx.strokeRect(e.x * tileSize + 1, e.y * tileSize + 1, tileSize - 2, tileSize - 2));
+    snake.forEach(e => cnvsCtx.strokeRect(e.x * tileWidth + 1, e.y * tileHeight + 1, tileWidth - 2, tileHeight - 2));
 }
 
 function drawBerries() {
     cnvsCtx.fillStyle = "red";
-    berries.forEach(e => cnvsCtx.fillRect(e.x * tileSize, e.y * tileSize, tileSize, tileSize));
+    berries.forEach(e => cnvsCtx.fillRect(e.x * tileWidth, e.y * tileHeight, tileWidth, tileHeight));
+}
+
+function gameOver() {
+    clearInterval(snakeInterval);
 }
 
 function moveSnake() {
@@ -95,13 +134,14 @@ function moveSnake() {
     if (!ateBerry) {
         snake.shift();
     }
-    console.log("x:" + snake[snake.length - 1].x + "y:" + snake[snake.length - 1].y);
 }
 
 function moveByKeyPress(e) { //testing
     changeDirection(e);
     gameLoop();
 }
+
+var direction = 'down';
 
 function changeDirection(e) {
     switch (e.key) {
@@ -126,4 +166,4 @@ function changeDirection(e) {
 
 // document.addEventListener("keydown", moveByKeyPress); // testing
 document.addEventListener("keydown", changeDirection); // "live"
-setInterval(gameLoop, 300); // live
+var snakeInterval = setInterval(gameLoop, 200); // live
